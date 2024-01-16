36cdbb18b19ac1404a9bf06884fdf640651bc4b8
Phyo-Alpha <77912443+Phyo-Alpha@users.noreply.github.com>
2024-01-16 16:43:48 +0800

    Added the static folder for favicon

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Body
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 
 @app.get("/")
